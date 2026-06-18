41777b581b26de0b86e86c543c237f733bd35f45
wanfeng-wf <2517514847@qq.com>
2026-06-18 15:23:31 +0800

    按 MJPEG 帧读取视频流

--- client/src/video.c
+++ client/src/video.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <sys/types.h>          /* See NOTES */
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <string.h>
@@ -53,10 +53,71 @@ void stop_mjpg_streamer(void)
 	mjpg_pid = -1;
 }
 
+static int read_line(int fd, char *buf, int size)
+{
+	int i = 0;
+	char c;
+
+	while (i < size - 1) {
+		int n = recv(fd, &c, 1, 0);
+		if (n <= 0) {
+			return n;
+		}
+
+		buf[i++] = c;
+		if (c == '\n') {
+			break;
+		}
+	}
+
+	buf[i] = '\0';
+	return i;
+}
+
+static int read_jpeg_frame(int fd, char *jpeg_buf, int jpeg_buf_size, int *jpeg_size)
+{
+	char line[256];
+	int content_length = 0;
+	int recv_size = 0;
+
+	// 先读到一个空行：第一次是HTTP响应头，后面是上一帧后的分隔行
+	while (read_line(fd, line, sizeof(line)) > 0) {
+		if (strcmp(line, "\r\n") == 0) {
+			break;
+		}
+	}
+
+	// 再读这一帧的MJPEG头，找到Content-Length
+	while (read_line(fd, line, sizeof(line)) > 0) {
+		if (strncmp(line, "Content-Length:", 15) == 0) {
+			content_length = atoi(line + 15);
+		}
+
+		if (strcmp(line, "\r\n") == 0) {
+			break;
+		}
+	}
+
+	if (content_length <= 0 || content_length > jpeg_buf_size) {
+		return -1;
+	}
+
+	while (recv_size < content_length) {
+		int n = recv(fd, jpeg_buf + recv_size, content_length - recv_size, 0);
+		if (n <= 0) {
+			return -1;
+		}
+		recv_size += n;
+	}
+
+	*jpeg_size = content_length;
+	return 0;
+}
+
 void *send_video_data(void *arg)
 {
-	//接收端口号·
-	int server_port=*(int *)arg;
+	// 接收udp端口号
+	int server_port = *(int *)arg;
 
 	//创建tcp socket
 	int video_sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -71,16 +132,14 @@ void *send_video_data(void *arg)
 	video_addr.sin_port = htons(VIDEOPORT);
     inet_pton(AF_INET, VIDEOADDR, &video_addr.sin_addr);
 
-
 	//发起连接
 	if (connect(video_sockfd, (struct sockaddr *)&video_addr, sizeof(video_addr)) < 0) {
 		perror("connect error");
 		exit(1);
 	}
-	//连接成功打印一个方便调试
 	printf("connect to mjpg-streamer success\n");
 
-	//创建udp socket 发送视频数据；
+	// 创建udp socket 发送视频数据；
 	int server_sockfd = socket(AF_INET,SOCK_DGRAM,0);
     if (server_sockfd == -1) {
 	    perror("udp socket error ");
@@ -92,7 +151,7 @@ void *send_video_data(void *arg)
     server_addr.sin_port=htons(server_port);
     inet_pton(AF_INET, SERVER_ADDR, &server_addr.sin_addr);
 
-	//发送post;
+	// 发送post;
     char *buf = (char *)malloc(BUFLEN);
     memset(buf, 0, BUFLEN);
     strcpy(buf, "GET /?action=stream\n");
@@ -101,51 +160,22 @@ void *send_video_data(void *arg)
 	    exit(1);
     }
 
-	//发送userinfo;任意两个字节
+	// 发送userinfo;任意两个字节
 	if (send(video_sockfd,"f\n", 2, 0) < 0) {
 		perror("send userinfo");
 		exit(1);
 	}
-	memset(buf, 0, BUFLEN);
-	if (recv(video_sockfd, buf, BUFLEN, 0) < 0) {
-		perror("recv head info");
 
-	}
 	int recv_size;
-	char *begin, *end;
-	char cont_len[10] = {0};
 	unsigned int frame_id = 1;
 	while (1) {
-		//接收头部信息
-		memset(buf, 0, BUFLEN);
-		memset(cont_len, 0, sizeof(cont_len));
-		recv_size = recv(video_sockfd, buf, BUFLEN, 0);
-		if(recv_size == -1) {
-			perror("recv");
-
-		}
-		//解析获取的信息
-		if(strstr(buf, "Content-Length")) {
-			begin=strstr(buf,"Content-Length");
-			end=strstr(buf,"X-Timestamp");
-			memcpy(cont_len,begin+16 ,end-2-begin-16);
-		} else {
-			continue;
+		// 读取一帧图片
+		if (read_jpeg_frame(video_sockfd, buf, BUFLEN, &recv_size) < 0) {
+			printf("read jpeg frame failed\n");
+			break;
 		}
 
-		memset(buf, 0, BUFLEN);
-		//接收帧数据
-		int picture_size = atoi(cont_len);
-		recv_size = 0;
-		while (recv_size < picture_size) {
-			int n = recv(video_sockfd, buf + recv_size, picture_size - recv_size, 0);
-			if (n <= 0) {
-				break;
-			}
-			recv_size += n;
-		}
-
-		//将一帧图片分片发送给公网服务器
+		//将一帧图片分片发送给服务器
 		int offset = 0;
 		while (offset < recv_size) {
 			unsigned char packet[sizeof(struct jpeg_udp_head) + UDP_DATA_SIZE];
@@ -174,10 +204,10 @@ void *send_video_data(void *arg)
 			usleep(1000);
 		}
 		frame_id++;
-
-		//接收尾部信息
-		memset(buf, 0, BUFLEN);
-		recv_size = recv(video_sockfd, buf, 1024, 0);
 	}
 
+	close(server_sockfd);
+	close(video_sockfd);
+	free(buf);
+	return NULL;
 }
